e18abf2032161b5194fc12d144b2f35adffe52b8
TyeBen123 <128625292+TyeBen123@users.noreply.github.com>
2026-01-25 23:46:45 +0800

    Update QrRelease.php

--- app/Models/QrRelease.php
+++ app/Models/QrRelease.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
-
+#Fixed the file name
 class QRRelease extends Model
 {
     use HasFactory;
