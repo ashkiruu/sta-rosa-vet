ec07b1d452002dd267492589fc98243856c6e377
jrfizap <jrfindaya@gmail.com>
2026-08-08 19:48:07 +0800

    For Render: Added fresh migration and seeding to entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Running database migrations..."
-php artisan migrate --force
+echo "Running database migrations and seeders..."
+# Runs migrations and executes DatabaseSeeder automatically
+php artisan migrate:fresh --seed --force
+# Uncomment this if Render is discarded
+#php artisan migrate --force
 
 echo "Starting Apache..."
 exec apache2-foreground
