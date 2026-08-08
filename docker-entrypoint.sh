5e21f4199effc30d83111aee3ed5d8123fd7897d
jrfizap <jrfindaya@gmail.com>
2026-08-08 20:40:48 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Clear all cached configurations, routes, and views dynamically on boot
+# (Get rid of this once Render is disabled)
+php artisan optimize:clear || true
+
 echo "Running database migrations and seeders..."
 # Runs migrations and executes DatabaseSeeder automatically
 php artisan migrate:fresh --seed --force
